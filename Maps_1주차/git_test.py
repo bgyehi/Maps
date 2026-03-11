636618bbc35d174e5f28cffe5f013078cae6de7b
성환 <hhhm6810@naver.com>
2026-03-11 13:53:46 +0900

    class로 더하기 한정 계산기

--- Maps_1주차/git_test.py
+++ Maps_1주차/git_test.py
@@ -1,1 +1,11 @@
-print("hello")
+class cal:
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+    def add(self):
+        result = self.first + self.second
+        return result
+
+a = cal()
+a.setdata(1,3)
+print(a.add())
